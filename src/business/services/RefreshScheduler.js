c9f5649cff1a804d047b8011bcfa95375599ac4d
leisingha <120764124+leisingha@users.noreply.github.com>
2025-11-24 17:52:29 -0500

    Implement FilterService, SearchService, Mapviewmanager and RefreshScheduler

---
+++ src/business/services/RefreshScheduler.js
@@ -0,0 +1,44 @@
+export class RefreshScheduler {
+    constructor() {
+        this.intervalSeconds = 30; // Default 30 seconds
+        this.timerId = null;
+        this.callback = null;
+    }
+
+    start(callback) {
+        this.stop(); // Ensure no existing timer
+        this.callback = callback;
+        this.timerId = setInterval(() => {
+            this.trigger();
+        }, this.intervalSeconds * 1000);
+    }
+
+    stop() {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+        }
+    }
+
+    setInterval(seconds) {
+        this.intervalSeconds = seconds;
+        if (this.isRunning()) {
+            // Restart with new interval
+            this.start(this.callback);
+        }
+    }
+
+    getInterval() {
+        return this.intervalSeconds;
+    }
+
+    trigger() {
+        if (this.callback) {
+            this.callback();
+        }
+    }
+
+    isRunning() {
+        return this.timerId !== null;
+    }
+}
